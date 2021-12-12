728469f6834d1f8432e59d7115aa2e427cd0efa0
AllaDihtiarova <AllaDihtiarova@gmail.com>
2021-12-12 20:42:34 +0200

    fix config eslint

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(process.env.APP_PORT);
