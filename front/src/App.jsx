3e5e9ecf53a1235c3e4471f42acc96bf50614c2a
AllaDihtiarova <AllaDihtiarova@gmail.com>
2022-01-09 23:04:41 +0200

    hw state

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,13 +6,13 @@ import user from 'data/userPost.json';
 export const App = () => {
   return (
     <>
-      <PostContainer
+      {/* <PostContainer
         name={user.name}
         location={user.location}
         avatar={user.avatar}
         text={user.text}
         stats={user.stats}
-      />
+      /> */}
       <ButtonContainer />
     </>
   );
